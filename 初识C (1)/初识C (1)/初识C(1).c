f9a3ddbe2101cc7989b00f16a69c306026e72293
yangxy01 <1271380537@qq.com>
2021-02-24 11:06:25 +0800

    Firstly cmomit

---
+++ 初识C (1)/初识C (1)/初识C(1).c
@@ -0,0 +1,14 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	scanf("%d", &a);
+	int b = 2;
+	if (a == 1)
+	if (b == 2)
+		printf("hehe\n");
+	else
+		printf("haha\n");
+	return 0;
+}
