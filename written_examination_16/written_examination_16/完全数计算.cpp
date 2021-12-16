b5d18f6ac0c81abb3cee18f39400e83d2e89183a
yangxy01 <1271380537@qq.com>
2021-12-16 10:00:23 +0800

    2021_12_16

---
+++ written_examination_16/written_examination_16/完全数计算.cpp
@@ -0,0 +1,24 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n, sum,count;
+//	while (cin >> n)
+//	{
+//		count = 0;
+//		for (int num = 2; num < n; num++)
+//		{
+//			sum = 0;
+//			for (int i = 1; i <= num / 2; i++)
+//			{
+//				if (num % i == 0)
+//					sum += i;
+//			}
+//			if (sum == num)
+//				count++;
+//		}
+//		cout << count << endl;
+//	}
+//	return 0;
+//}
