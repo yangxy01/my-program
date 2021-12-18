039daeb572eeed7dc91f5fc5b3a455edb54dc35a
yangxy01 <1271380537@qq.com>
2021-12-18 09:15:00 +0800

    2021_12_18

---
+++ written_examination_20/written_examination_20/字符串反转.cpp
@@ -0,0 +1,21 @@
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//
+//int main()
+//{
+//	string str;
+//	cin >> str;
+//	auto start = str.begin();
+//	auto end = str.end() - 1;
+//	while (start <= end)
+//	{
+//		char ch = *start;
+//		*start = *end;
+//		*end = ch;
+//		start++;
+//		end--;
+//	}
+//	cout << str << endl;
+//	return 0;
+//}
