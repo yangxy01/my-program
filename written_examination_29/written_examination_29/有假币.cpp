d6b22e6efa289ea96bd42029ec3e7a5d9258da6d
yangxy01 <1271380537@qq.com>
2022-01-02 14:38:57 +0800

    2022_1_2

---
+++ written_examination_29/written_examination_29/有假币.cpp
@@ -0,0 +1,33 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	long long n;
+//	while (cin >> n)
+//	{
+//		
+//		if (n == 0)
+//			break;
+//		if (n == 1)
+//		{
+//			cout << 0 << endl;
+//		}
+//
+//		int count = 1;
+//		while (n > 3)
+//		{
+//			if (n % 3)
+//				n = n / 3 + 1;
+//			else
+//				n /= 3;
+//			count++;
+//		}
+//		cout << count <<endl;
+//
+//
+//		
+//		
+//	}
+//	return 0;
+//}
