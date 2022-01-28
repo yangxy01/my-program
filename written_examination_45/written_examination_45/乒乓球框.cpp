d9209af650900a071b9d938ac9ca6e62192352c4
yangxy01 <1271380537@qq.com>
2022-01-28 08:56:25 +0800

    2022_1_28

---
+++ written_examination_45/written_examination_45/乒乓球框.cpp
@@ -0,0 +1,29 @@
+//#include<iostream>
+//#include<string>
+//using namespace std;
+////ABCDFYE CDE<br/>ABCDGEAS CDECDE
+//int main()
+//{
+//	string s1, s2;
+//	while (cin >> s1 >> s2)
+//	{
+//		int flag = 1;
+//		for (auto e : s2)
+//		{
+//			auto pos = s1.find(e);
+//			if (pos == string::npos)
+//			{
+//				cout << "No" << endl;
+//				flag = 0;
+//				break;
+//			}
+//			else
+//			{
+//				s1[pos] = '0';
+//			}
+//		}
+//		if(flag)
+//			cout << "Yes" << endl;
+//	}
+//	return 0;
+//}
